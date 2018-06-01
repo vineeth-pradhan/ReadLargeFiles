29ff070f03303cd227e522dfa6e4fa5ad3ae258e
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2018-06-01 15:55:26 +0530

    [pipe] Refactor

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const fs = require('fs');
 const atob = require('atob');
 const sliceSize = 4096 * 1024;
-const readPath = process.env.PWD+'/image-greater-than-25-mb.jpg';
-const writePath = process.env.PWD+'/image-greater-than-25-mb-complete.jpg';
+const readPath = process.env.PWD+'/sample-large-file.jpg';
+const writePath = process.env.PWD+'/complete.jpg';
 
 fs.stat(readPath, (err, stat) => {
-  function readBuffer(start) {
-    const nextSlice = sliceSize + start + 1;
+  function readBuffer() {
     const readStream = fs.createReadStream(readPath, { encoding: 'binary' });
     const writeStream = fs.createWriteStream(writePath, { flags: 'a', encoding: 'binary' });
     readStream.pipe(writeStream)
-    readStream.on('unpipe', () => {
-      readBuffer(nextSlice);
-    })
   }
-  readBuffer(0);
+  readBuffer();
 });
