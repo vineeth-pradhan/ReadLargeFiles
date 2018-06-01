ff70102362aebdf9dcb8eea73f6c64ddfb45c674
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2018-06-01 20:35:45 +0530

    [read-chunk] Read chunks of data using createReadStream

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
+// Libraries
 const fs = require('fs');
 const atob = require('atob');
-const sliceSize = 4096 * 1024;
-const readPath = process.env.PWD+'/image-greater-than-25-mb.jpg';
-const writePath = process.env.PWD+'/image-greater-than-25-mb-complete.jpg';
 
-fs.stat(readPath, (err, stat) => {
-  const writeStream = fs.createWriteStream(writePath, { flags: 'a', encoding: 'binary' });
-  function readBuffer(offset) {
-    const nextSlice = sliceSize + offset + 1;
-    fs.open(readPath, 'r', (err, fd) => {
-      if(offset < stat.size) {
-        fs.read(fd, Buffer.alloc(sliceSize), 0, sliceSize, offset, (err, bytesRead, buffer) => {
-          writeStream.write(buffer, () => {
-            fs.close(fd, () => {
-              if(nextSlice < stat.size) { readBuffer(nextSlice); }
-              else { return; }
-            });
-          })
-        });
-      }
-    });
-  }
-  readBuffer(0);
-});
+// Constants
+const sliceSize = 4096 * 1024;
+const readPath = process.env.PWD+'/sample-large-file.jpg';
+const writePath = process.env.PWD+'/complete.jpg';
+
+// Initializing streams
+const writeStream = fs.createWriteStream(writePath, { flags: 'a', encoding: 'binary' });
+const readStream = fs.createReadStream(readPath, { encoding: 'base64', start: 0 });
+
+readStream.read(sliceSize);
+readStream.on('data', data => { writeStream.write(atob(data)); });
