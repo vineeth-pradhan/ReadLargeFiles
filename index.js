6dd216f0c52b5c6ea4b5478e7320a22b6e53a100
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2018-06-01 13:32:17 +0530

    [master] Reading file with fs.read

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const fs = require('fs');
+const atob = require('atob');
 const sliceSize = 4096 * 1024;
-const path = process.env.PWD+"/image-greater-than-25-mb.jpg";
-var size = 0;
-fs.stat(path, (err, stat) => {
-  fs.open(path, 'r', (err, fd) => {
-    function readBuffer(offset) {
-      const nextSlice = offset + sliceSize + 1;
+const readPath = process.env.PWD+'/image-greater-than-25-mb.jpg';
+const writePath = process.env.PWD+'/image-greater-than-25-mb-complete.jpg';
+
+fs.stat(readPath, (err, stat) => {
+  const writeStream = fs.createWriteStream(writePath, { flags: 'a', encoding: 'binary' });
+  function readBuffer(offset) {
+    const nextSlice = sliceSize + offset + 1;
+    fs.open(readPath, 'r', (err, fd) => {
       if(offset < stat.size) {
-        fs.read(fd, Buffer.alloc(sliceSize, 0, 'base64'), 0, sliceSize, offset, (err, bytesRead, buffer) => {
-          console.log(stat.size - size)
-          console.log(bytesRead)
-          size = bytesRead + size;
-          if(offset < stat.size) {
-            console.log(buffer);
-            readBuffer(nextSlice);
-          }
+        fs.read(fd, Buffer.alloc(sliceSize), 0, sliceSize, offset, (err, bytesRead, buffer) => {
+          writeStream.write(buffer, () => {
+            fs.close(fd, () => {
+              if(nextSlice < stat.size) { readBuffer(nextSlice); }
+              else { return; }
+            });
+          })
         });
       }
-    }
-
-    readBuffer(0);
-  });
+    });
+  }
+  readBuffer(0);
 });
