f0d0d99f029f0bd728877ac78e02e41812fdd678
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2018-06-01 15:37:42 +0530

    [pipe] Piping data

--- index.js
+++ index.js
@@ -5,21 +5,14 @@ const readPath = process.env.PWD+'/image-greater-than-25-mb.jpg';
 const writePath = process.env.PWD+'/image-greater-than-25-mb-complete.jpg';
 
 fs.stat(readPath, (err, stat) => {
-  const writeStream = fs.createWriteStream(writePath, { flags: 'a', encoding: 'binary' });
-  function readBuffer(offset) {
-    const nextSlice = sliceSize + offset + 1;
-    fs.open(readPath, 'r', (err, fd) => {
-      if(offset < stat.size) {
-        fs.read(fd, Buffer.alloc(sliceSize), 0, sliceSize, offset, (err, bytesRead, buffer) => {
-          writeStream.write(buffer, () => {
-            fs.close(fd, () => {
-              if(nextSlice < stat.size) { readBuffer(nextSlice); }
-              else { return; }
-            });
-          })
-        });
-      }
-    });
+  function readBuffer(start) {
+    const nextSlice = sliceSize + start + 1;
+    const readStream = fs.createReadStream(readPath, { encoding: 'binary' });
+    const writeStream = fs.createWriteStream(writePath, { flags: 'a', encoding: 'binary' });
+    readStream.pipe(writeStream)
+    readStream.on('unpipe', () => {
+      readBuffer(nextSlice);
+    })
   }
   readBuffer(0);
 });
